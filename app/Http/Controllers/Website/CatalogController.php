591b9f9c03fb69273a93e3b22b5cdcf43c7eca07
saichudin <m.saichudin@gmail.com>
2020-09-21 10:54:32 +0700

    seeder user

---
+++ app/Http/Controllers/Website/CatalogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Website;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CatalogController extends Controller
+{
+    public function index()
+    {
+        dd('aaa');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Website\CatalogController;
+use App\Http\Controllers\Admin\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('catalog', [CatalogController::class, 'index']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('book', [BookController::class, 'index']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
